95b91ff2a446c36ccf24be372933a834f18bdea9
KOFLazycat <KOFLAzycat@163.com>
2025-03-09 11:48:57 +0800

    1. 优化label缓慢增加数值动效，增加放大、缩小Q弹效果

--- vfx/tween_label/tween_label.gd
+++ vfx/tween_label/tween_label.gd
@@ -7,7 +7,12 @@ class_name TweenLabel extends Node2D
 
 func _ready() -> void:
 	var tween: Tween = create_tween()
-	tween.tween_method(update_label, from_num, to_num, 5.0).set_trans(Tween.TRANS_LINEAR)
+	tween.tween_method(update_label, from_num, to_num, 0.2).set_trans(Tween.TRANS_LINEAR)
+	# 放大到 1.5 倍，快速完成（0.2秒）
+	tween.parallel().tween_property(label, "scale", Vector2(1.5, 1.5), 0.2).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+
+	# 缩放到原始尺寸，带有弹性效果（0.5秒）
+	tween.tween_property(label, "scale", Vector2(1.0, 1.0), 0.5).set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_OUT)
 
 
 func update_label(v: int) -> void:
